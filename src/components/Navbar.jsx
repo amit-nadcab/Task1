39a2f9d945b039632ac64cd830f474b47bc16f88
amit-nadcab <amitnadcab@gmail.com>
2023-04-05 16:14:37 +0530

    replace class with className

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,29 @@ export default function Navbar() {
   return (
     <>
       <div className="text-white sticky-top" style={{ background: "#1b3179" }}>
-        <div class="topbar">
-          <div class="toggle">
+        <div className="topbar">
+          <div className="toggle">
             <ion-icon name="menu-outline"></ion-icon>
           </div>
 
-          <div class=" bg-dark rounded-2 p-3 px-3">
-            <div>Credit Balance: <span className="bg-white text-dark rounded-5 p-1 ms-2 px-3">1233131</span></div>
+          <div className=" bg-dark rounded-2 p-3 px-3">
+            <div>
+              Credit Balance:{" "}
+              <span className="bg-white text-dark rounded-5 p-1 ms-2 px-3">
+                1233131
+              </span>
+            </div>
           </div>
-          <div class="bg-dark rounded-2 p-3 px-3">
-            <div>Debit Balance : <span className="bg-white text-dark rounded-5 p-1 ms-2 px-3">1233131</span></div>
+          <div className="bg-dark rounded-2 p-3 px-3">
+            <div>
+              Debit Balance :{" "}
+              <span className="bg-white text-dark rounded-5 p-1 ms-2 px-3">
+                1233131
+              </span>
+            </div>
           </div>
-         
-          
-          {/* <div class="user">
+
+          {/* <div className="user">
             <div className='col-md-4'><div>Debit Balance</div></div>
             </div> */}
         </div>

4416074354ae6b20763a2b02eaad8cb99116bc92
amit-nadcab <amitnadcab@gmail.com>
2023-04-08 17:10:53 +0530

    change setting icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Link,NavLink } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 import {
   AccountBalanceOutlined,
@@ -9,9 +9,10 @@ import {
   FingerprintOutlined,
   List,
   PhoneAndroid,
+  Settings,
+  SettingsAccessibility,
 } from "@mui/icons-material";
 export default function Sidebar() {
-
   useEffect(() => {
     let list = document.querySelectorAll(".navigation li");
 
@@ -38,105 +39,105 @@ export default function Sidebar() {
   return (
     <>
       <div className="navigation">
-          <ul>
-            <li>
-              <Link href="#">
-                {/* <span className="icon">
+        <ul>
+          <li>
+            <Link href="#">
+              {/* <span className="icon">
                   <ion-icon name="logo-apple"></ion-icon>
                 </span> */}
-                <span className="title">
-                  <img width={150} src="/images/Any_pe_logo.png" alt="logo" />
-                </span>
-              </Link>
-            </li>
+              <span className="title">
+                <img width={150} src="/images/Any_pe_logo.png" alt="logo" />
+              </span>
+            </Link>
+          </li>
 
-            <span className="d-flex justify-content-center">
-              <div className="text-center">
-                <span>Golu Meena</span>
-                <p>Silver Partner | RT343817</p>
-              </div>
-            </span>
+          <span className="d-flex justify-content-center">
+            <div className="text-center">
+              <span>Golu Meena</span>
+              <p>Silver Partner | RT343817</p>
+            </div>
+          </span>
 
-            <li>
-              <NavLink to="/dashboard">
-                <span className="icon">
-                  <DashboardCustomize />
-                </span>
-                <span className="title">Dashboard</span>
-              </NavLink>
-            </li>
+          <li>
+            <NavLink to="/dashboard">
+              <span className="icon">
+                <DashboardCustomize />
+              </span>
+              <span className="title">Dashboard</span>
+            </NavLink>
+          </li>
 
-            <li>
-              <NavLink to="#">
-                <span className="icon">
-                  <AccountCircleRounded />
-                </span>
-                <span className="title">Profile</span>
-              </NavLink>
-            </li>
+          <li>
+            <NavLink to="#">
+              <span className="icon">
+                <AccountCircleRounded />
+              </span>
+              <span className="title">Profile</span>
+            </NavLink>
+          </li>
 
-            <li>
-              <NavLink to="/BEPS">
-                <span className="icon">
-                  <PhoneAndroid />
-                </span>
-                <span className="title">BEPS</span>
-              </NavLink>
-            </li>
+          <li>
+            <NavLink to="/BEPS">
+              <span className="icon">
+                <PhoneAndroid />
+              </span>
+              <span className="title">BEPS</span>
+            </NavLink>
+          </li>
 
-            <li>
-              <NavLink to="/AEPS">
-                <span className="icon">
-                  <FingerprintOutlined />
-                </span>
-                <span className="title">AEPS</span>
-              </NavLink>
-            </li>
+          <li>
+            <NavLink to="/AEPS">
+              <span className="icon">
+                <FingerprintOutlined />
+              </span>
+              <span className="title">AEPS</span>
+            </NavLink>
+          </li>
 
-            <li>
-              <Link href="#">
-                <span className="icon">
-                  <AccountBalanceOutlined />
-                </span>
-                <span className="title">Payout</span>
-              </Link>
-            </li>
+          <li>
+            <Link href="#">
+              <span className="icon">
+                <AccountBalanceOutlined />
+              </span>
+              <span className="title">Payout</span>
+            </Link>
+          </li>
 
-            <li>
-              <Link href="#">
-                <span className="icon">
-                  <AccountBalanceOutlined />
-                </span>
-                <span className="title">DMT</span>
-              </Link>
-            </li>
+          <li>
+            <Link href="#">
+              <span className="icon">
+                <AccountBalanceOutlined />
+              </span>
+              <span className="title">DMT</span>
+            </Link>
+          </li>
 
-            <li>
-              <Link href="#">
-                <span className="icon">
-                  <AccountBalanceWallet />
-                </span>
-                <span className="title">Wallet</span>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <span className="icon">
-                  <List />
-                </span>
-                <span className="title">Report</span>
-              </Link>
-            </li>
-            <li>
-              <Link href="#">
-                <span className="icon">
-                  <ion-icon name="log-out-outline"></ion-icon>
-                </span>
-                <span className="title">Account Settings</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+          <li>
+            <Link href="#">
+              <span className="icon">
+                <AccountBalanceWallet />
+              </span>
+              <span className="title">Wallet</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <span className="icon">
+                <List />
+              </span>
+              <span className="title">Report</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="#">
+              <span className="icon">
+                <Settings />
+              </span>
+              <span className="title">Account Settings</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
-  )
+  );
 }
